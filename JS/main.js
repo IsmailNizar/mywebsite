04065c82c1d0b6aa9d0c06e967a67183c348cbb5
Ismail <ismail.96.nizar@gmail.com>
2020-09-14 17:31:25 +0200

    update v 2.0

--- JS/main.js
+++ JS/main.js
@@ -19,49 +19,52 @@ onReady(function () {
 });
 
 // Wrap every letter in a span
-
-function ex(){
-  var textWrapper = document.querySelector('.ml1');
-  textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-anime.timeline({repeat: 1})
-  .add({
-    targets: '.ml1 .letter',
-    scale: [4,1],
-    opacity: [0,1],
-    translateZ: 0,
-    easing: "easeOutExpo",
-    duration: 1000,
-    delay: (el, i) => 70*i
-  });
+function wrapLetters(className){
+  setTimeout(function wrap(){
+    var textWrapper = document.querySelector(className);
+    textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+    className+=' .letter';
+    anime.timeline({repeat: 1})
+    
+    .add({
+      targets: className,
+      scale: [4,1],
+      opacity: [0,1],
+      translateZ: 0,
+      easing: "easeOutExpo",
+      duration: 1000,
+      delay: (el, i) => 70*i
+    });
+  },1600);
 }
 
-function ex1(){
-  var textWrapper = document.querySelector('.ml2');
-  textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-anime.timeline({repeat: 1})
-  .add({
-    targets: '.ml2 .letter',
-    scale: [4,1],
-    opacity: [0,1],
-    translateZ: 0,
-    easing: "easeOutExpo",
-    duration: 1000,
-    delay: (el, i) => 70*i
-  });
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+setInterval(() => {
+showSlides(slideIndex += 1);
+}, 5000);  
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+function currentSlide(n) {
+  showSlides(slideIndex = n);
 }
 
-function ex2(){
-  var textWrapper = document.querySelector('.ml3');
-  textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-anime.timeline({repeat: 1})
-  .add({
-    targets: '.ml3 .letter',
-    scale: [4,1],
-    opacity: [0,1],
-    translateZ: 0,
-    easing: "easeOutExpo",
-    duration: 1000,
-    delay: (el, i) => 70*i
-  });
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
 }
-
